ac2305827eb4b27644bd6518e5ffa931adfce7ce
anthonny tineo <anthonny@MacBook-Pro-de-anthonny.local>
2021-05-17 20:56:31 -0400

    practica de app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './Navbar'
+import Numero from './Numero'
+import Post from './Post';
+import './App.css';
+
+function App() {
+
+  const propsIniciales = [
+    {nick: "Re-On", mensaje: "Soy Re-on"},
+    {nick: "Fracisco", mensaje: "Soy Francisco"},
+    {nick: "Juan", mensaje: "Soy Juan"},
+    {nick: "Pedro", mensaje: "Soy Pedro"}
+  ]
+
+
+  return (
+    <div className="App">
+      {
+        propsIniciales.map(e => <Post nick={e.nick} mensaje={e.mensaje}/>)
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+    const Navbar = () => {
+
+
+        
+        return(
+            <div>
+                <h1>Hola Mundo</h1>
+            </div>
+        )
+    }
+    
+
+
+
+export default Navbar;
